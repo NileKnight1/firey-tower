c81df1c3a506185e9c773bc902de5c2a4f0a3611
NileKnight0 <ammartube777@gmail.com>
2026-09-19 05:25:06 +0300

    changedgame style and themes

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,29 +5,50 @@ func _ready() -> void:
 	start_game()
 
 func start_game():
+	game_running = 1
 	spawn_apply()
+
+
+var cam_move_speed = 0.2
 
 func spawn_apply():
 	if $platforms.get_child_count():
 		pass
 	
-
+var good = 0
 func _process(delta: float) -> void:
-	$cam.position.y -= 0.2
+	if !game_running: return
+	$cam.position.y -= cam_move_speed
 	
 	if ($cam.position.y - $player.position.y) > 100:
-		$cam.position.y -= 70
+		good = 1
+		var temp = $cam.position.y
+		var tween = create_tween()
+		tween.tween_property($cam, "position:y", temp-70, 0.3)
+	else:
+		good = 0
 	
+	#print(good)
 	#print(cur_y)
 	#print($cam.position.y)
 	
 	if abs(cur_y-$cam.position.y) < 500:
 		spawn_platform()
-	print($platforms.get_child_count())
+	#print($platforms.get_child_count())
 	if $platforms.get_child_count() > 10:
 		$platforms.get_child(0).queue_free()
 	
-	pass
+	if ( $player.position.y - $cam.position.y) >= 290:
+		print("dead")
+		game_over()
+	
+	#print($cam.position.y)
+	#print($player.position.y)
+
+var game_running = 0
+
+func game_over():
+	game_running = 0
 
 var cur_y = 180
 
@@ -35,22 +56,28 @@ var cur_y = 180
 # panel size.y 325:540
 
 
-@onready var ref: Panel = $refs/Panel
 
+@onready var ref_scene = preload("res://scenes/panel.tscn")
 func spawn_platform():
 	#var temp = randi_range(0, $refs.get_child_count()-1)
 	#temp = $refs.get_child(temp).duplicate()
 	
-	var temp = ref.duplicate()
+	
+	var temp = ref_scene.instantiate()
 	temp.position.x = randf_range(-480, 125)
 	temp.size.y = randf_range(325, min(540, 450-temp.position.x))
-	#print(temp.position.x)
+	#print(temp. position.x)
 	#print(540-temp.position.x)
 	
-	temp.get_child(0).get_child(0).shape.b.x = temp.size.y 
+	var collision_shape = temp.get_child(0).get_child(0)
+	collision_shape.shape = collision_shape.shape.duplicate()
+	collision_shape.shape.b.x = temp.size.y
 	
 	temp.position.y = cur_y
-	cur_y -= 180
+	cur_y -= 260
 	
 	$platforms.add_child(temp)
 	
+func _on_dif_timeout() -> void:
+	#cam_move_speed += 0.4
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,15 +2,21 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -1300.0
-
+const JUMP_VELOCITY = -1300
+const GRAVITY_UP = 3.0
+const GRAVITY_DOWN = 6.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta *5
-
+		if velocity.y < 0:
+			velocity += get_gravity() * delta * GRAVITY_UP
+		else:
+			velocity += get_gravity() * delta * GRAVITY_DOWN
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= 0.5
 
 	var direction := Input.get_axis("left", "right")
 	if direction:
